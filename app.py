3170d7f2d6bf85b3c273d34970b060dde5a9acc5
evancthome <evancthome@gmail.com>
2025-02-19 17:53:32 -0500

    fix url2

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 # Allow requests from both the development and production domains
 CORS(app, origins=[
-    'https://www.letterquared.com'
+    'https://www.lettersquared.com'
 ])
 
 def load_word_list():
